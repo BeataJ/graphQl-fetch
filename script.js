bc80b0f268562d0f46dee4be95f8823a1e643b4b
beata <beata@emc2wave.com>
2019-10-07 10:23:10 -0700

    add func getContinentCountries

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
 const continentSelect = document.getElementById('continent-select');
 
-fetch("https://countries.trevorblades.com/", {
-  method: "POST",
-  headers: { "Content-type": "application/json" },
-  body: JSON.stringify({
-    query: `
-      query {
-        continents {
-          name
-          code
-        }
-      }
-    `
-  })
-})
-  .then(res => res.json())
+queryFetch(`
+  query { 
+    continents {
+      name
+      code
+    }
+  }
+  `
+)
   .then(data => {
     data.data.continents.forEach(continent => {
       const option = document.createElement('option');
@@ -23,3 +17,36 @@ fetch("https://countries.trevorblades.com/", {
       continentSelect.append(option);
     });
   });
+
+  continentSelect.addEventListener('change', async e => {
+    const continentCode = e.target.value;
+    const countries = await getContinentCountries(continentCode)
+    console.log(countries);
+  })
+
+
+  function getContinentCountries(continentCode) {
+    return queryFetch(`
+      query getCountries($code: String) {
+        continent(code: $code) {
+          countries {
+            name
+          }
+        }
+      }
+    `, { code: continentCode }).then(data => {
+      return data.data.continent.countries
+    })
+  }
+
+
+  function queryFetch(query, veriables) {
+    return fetch("https://countries.trevorblades.com/", {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({
+        query: query,
+        variables: veriables
+      })
+    }).then(res => res.json())
+  }
